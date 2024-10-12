c7c709c67b2bd63581e25039f8b6dc0603a5bc97
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-12 17:35:54 +0530

    Feat: Added Wallet Adapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Button } from "./components/ui/button";
+import { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 function App() {
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
     return (
         <>
-            <Button>Shadcn Init</Button>
+            <ConnectionProvider endpoint={endpoint}>
+                <WalletProvider wallets={[]} autoConnect>
+                    <WalletModalProvider>
+                        <header className="app-header bg-white shadow p-4 flex justify-between items-center">
+                            <WalletMultiButton className="wallet-button" />
+                            <WalletDisconnectButton className="wallet-button" />
+                        </header>
+                    </WalletModalProvider>
+                </WalletProvider>
+            </ConnectionProvider>
         </>
     );
 }
